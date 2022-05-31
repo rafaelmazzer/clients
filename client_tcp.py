ccf1d78ef89780262f198c43c5f3e0e4542518a5
Rafael Mazzer <49206777+rafaelmazzer@users.noreply.github.com>
2022-05-31 19:20:50 -0300

    1o commit

---
+++ client_tcp.py
@@ -0,0 +1,11 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.settimeout(1)
+try:
+    client.connect(('google.com', 80))
+    client.send(b'GET / HTTP/1.1\nHost: www.google.com\n\n\n')
+    pacotes_recebidos = client.recv(1024).decode()
+    print(pacotes_recebidos)
+except:
+    print('Um erro ocorreu.')
