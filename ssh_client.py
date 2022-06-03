7bf579c3fe027b17eb38045555c1c8d18d2fa7e5
Rafael Mazzer <49206777+rafaelmazzer@users.noreply.github.com>
2022-06-02 21:17:56 -0300

    ssh_server finalizado

--- ssh_client.py
+++ ssh_client.py
@@ -1,3 +1,4 @@
+from sys import stderr, stdout
 import paramiko
 
 host = '127.0.0.1'
@@ -7,3 +8,12 @@ passwd = 'kali'
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=user, password=passwd)
+
+while True:
+    stdin, stdout, stderr = client.exec_command(input('Comando: '))
+    for line in stdout.readlines():
+        print(line.strip())
+    
+    erros = stderr.readlines()
+    if erros:
+        print(erros)
